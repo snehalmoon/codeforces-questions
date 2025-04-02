3d7f97adb0d725a22b5cfc50a5f74cc70387e2b6
snehal moon <127291185+snehalmoon@users.noreply.github.com>
2025-04-02 13:57:02 +0530

    Add files via upload

---
+++ CF1896A.java
@@ -0,0 +1,22 @@
+package javabootcamp;
+
+import java.util.Scanner;
+
+public class CF1896A {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t--> 0) {
+			int n=sc.nextInt();
+			boolean check=false;
+			
+			for(int i=0;i<n;i++) {
+				int num=sc.nextInt();
+				if(i==0 && num==1) check=true;
+			}
+			System.out.println(check?"Yes":"No");
+		}
+	}
+
+}
